b59e6a64025938280a9b9be157feca650fb564de
unknown <paulooliveira133@gmail.com>
2026-05-14 18:46:56 -0300

    fix: hydration error

--- src/app/s/[slug]/layout.tsx
+++ src/app/s/[slug]/layout.tsx
@@ -1,3 +1,9 @@
-export default function SlugLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+export default function SlugLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { slug: string };
+}) {
+  return <main className="min-h-screen">{children}</main>;
 }
